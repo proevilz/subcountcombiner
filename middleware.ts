09396f2d4d700ffaec4efe764694a75076a01672
ProEvilz <ashley1212@live.co.uk>
2023-12-03 08:01:56 +0000

    new token

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,6 @@ import type { NextRequest } from "next/server"
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log(
-    "firings",
-    request.url,
-    request.url.includes("mal5a37a816-3853-4300-bbd5-9fed001b3545")
-  )
   if (
     request.url.includes("mal5a37a816-3853-4300-bbd5-9fed001b3545") ||
     request.url.includes("ark5a37a816-3853-4300-bbd5-9fed001b3545")
